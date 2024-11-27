a867f4d35a99745db281f00f1e3566bf6d392f5f
DeejMags <152266665+DeejMags@users.noreply.github.com>
2024-11-27 22:36:59 +0800

    Delete app directory

--- app/src/main/java/data/CityUiState.kt
+++
@@ -1,7 +0,0 @@
-package com.example.mycityapp.data
-
-data class CityUiState(
-    val categories: List<Category> = listOf(),
-    val currentCategory: Category = DataSource.defaultCategory,
-    val currentReccomendation: Reccomendation = DataSource.defaultReccomendation
-)
